5c3abc1811a15f36420bf19a55a304c711d772b4
Radek <rajkonkret660@gmail.com>
2025-12-15 15:37:55 +0100

    [1 23] update funkcja4.py

---
+++ dzien1/funkcja5_rekurencja.py
@@ -0,0 +1,22 @@
+# funkcja rekurencyjna
+# funkcja wywołuje sama siebie
+
+def silnia(n):
+    if n == 0:
+        return 1
+    else:
+        return n * silnia(n - 1)
+
+
+print(silnia(5))  # 120
+
+
+def nwd(a, b):
+    if b == 0:
+        return a
+    return nwd(b, a % b)  # % - modulo, reszta z dzielenia
+
+
+print(nwd(48, 18))  # 6
+
+
