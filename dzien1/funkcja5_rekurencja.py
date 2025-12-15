e96f1096147e686aed24c547a43498d0994977f0
Radek <rajkonkret660@gmail.com>
2025-12-15 15:39:42 +0100

    [1 24] update funkcja4.py

--- dzien1/funkcja5_rekurencja.py
+++ dzien1/funkcja5_rekurencja.py
@@ -19,4 +19,13 @@ def nwd(a, b):
 
 print(nwd(48, 18))  # 6
 
+nasted_data = {
+    "a": 1,
+    "b": {
+        "c": 2,
+        "d": [3, 4, {"e": 5}]
+    },
+    "f": [6, 7]
+}
 
+# zsumowac wszystkie wartości ze słownika
