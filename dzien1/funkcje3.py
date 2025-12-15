040edd63a58c05d7220d78d15f9235724e41a162
Radek <rajkonkret660@gmail.com>
2025-12-15 14:20:12 +0100

    [1 17] update funkcja3.py.py

---
+++ dzien1/funkcje3.py
@@ -0,0 +1,8 @@
+# funkcje wyższego rzędu
+
+def witaj(imie):
+    return f"Miło Cię widzieć {imie}"
+
+
+def konkurs(imie, miejsce, punkty):
+    return f"Uczestnik konkursu: {imie}, miejsce: {miejsce}, liczba punktów: {punkty}"
