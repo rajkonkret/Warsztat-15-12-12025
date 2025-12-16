c25fdb1a328d883ada36098515eb683acbd93faf
Radek <rajkonkret660@gmail.com>
2025-12-16 12:37:58 +0100

    [2 20] update funkcja12_generator.py

--- dzien2/fun12_gen3.py
+++ dzien2/fun12_gen3.py
@@ -40,3 +40,33 @@ c = combined(c_u, c_d)
 
 for i in c:
     print(i)
+
+dane = [x for x in range(5)]
+print(dane)  # [0, 1, 2, 3, 4]
+print(type(dane))  # <class 'list'>
+
+# generator skłądane
+dane2 = (x for x in [1, 2, 3, 4, 5])
+print(type(dane2))  # <class 'generator'>
+print(dane2)  # <generator object <genexpr> at 0x0000026FAF452A80>
+
+print(next(dane2))  # 1
+print(next(dane2))  # 2
+print(next(dane2))  # 3
+print(next(dane2))  # 4
+print(next(dane2))  # 5
+
+
+# print(next(dane2))  # StopIteration
+
+def generator3():
+    for x in [1, 2, 3, 4, 5]:
+        yield x
+
+
+g3 = generator3()
+print(next(g3))  # 1
+print(next(g3))  # 2
+print(next(g3))  # 3
+print(next(g3))  # 4
+print(next(g3))  # 5
