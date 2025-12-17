271292d21fabf4190b8e00dfb094ad738a605ef6
Radek <rajkonkret660@gmail.com>
2025-12-17 12:06:41 +0100

    [3 18] update kl4

--- dzien3/kl4.py
+++ dzien3/kl4.py
@@ -33,3 +33,23 @@ a2['age'] = 34
 print(a2)  # {'age': 34}
 print(a2['name'])
 print(a2)  # {'age': 34, 'name': 0}
+
+
+# zmienia klucze na duże litery
+class CaseInsensitiveDict(dict):
+    def __missing__(self, key):
+        # return self.get(key.lower())
+        if isinstance(key, str):
+            return self[key.lower()]
+        return key
+
+
+c1 = CaseInsensitiveDict()
+c1['name'] = "Radek"
+print(c1)  # {'name': 'Radek'}
+print(c1['Name'])  # Radek
+# c1[1] = 10
+print(c1[1])  # AttributeError: 'int' object has no attribute 'lower'
+# 1
+c1[1] = "tekst"
+print(c1)  # {'name': 'Radek', 1: 'tekst'}
