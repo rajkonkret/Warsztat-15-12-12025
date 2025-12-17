3d499c8cb06f36386db5512d8efc04ce1f66e0a1
Radek <rajkonkret660@gmail.com>
2025-12-17 10:40:22 +0100

    [3 7] update kl1

--- dzien3/kl1.py
+++ dzien3/kl1.py
@@ -52,6 +52,9 @@ class MyFirstClass:
     def __str__(self):
         return f"{self.x, self.y}"
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}{self.x, self.y}"
+
 
 ob = MyFirstClass()
 print(MyFirstClass.__doc__)  # Klasa w pythonie
@@ -76,3 +79,27 @@ print(point1)  # (0, 0)
 point2 = MyFirstClass()
 point2.move(78, 45)
 print(point2)  # (78, 45)
+
+print(point1.calculate(point2))  # 90.0499861188218
+
+point3 = MyFirstClass(43, 21)
+point4 = MyFirstClass(13, 89)
+print(point3)  # (43, 21)
+print(point4)  # (13, 89)
+
+print(point3.calculate(point2))  # 42.43819034784589
+
+lista = [point1, point2, point3, point4, ob]
+print(lista)
+# __str__ -> print(), str()
+# [<__main__.MyFirstClass object at 0x0000029DE7E21810>,
+# <__main__.MyFirstClass object at 0x0000029DEA021310>,
+# <__main__.MyFirstClass object at 0x0000029DDF6FDCD0>,
+# <__main__.MyFirstClass object at 0x0000029DE7DCEEA0>,
+# <__main__.MyFirstClass object at 0x0000029DE9FBC830>]
+# __repr__  - bardziej użyteczna dla programisty
+# [MyFirstClass(0, 0),
+# MyFirstClass(78, 45),
+# MyFirstClass(43, 21),
+# MyFirstClass(13, 89),
+# MyFirstClass(0, 0)]
