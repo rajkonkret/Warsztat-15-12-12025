47e84deec6104089113e02257ca75d3eb2f8f784
Radek <rajkonkret660@gmail.com>
2025-12-17 14:29:05 +0100

    [3 26] update kl8

--- dzien3/kl8_zad2.py
+++ dzien3/kl8_zad2.py
@@ -7,6 +7,45 @@
 # dodac kalsę Library i ew. usera
 # title, author, isbn
 # obsłużyć błędy
+from functools import wraps
+
+
+def audit(action: str):
+    """
+    Dekorator dopisujący wpis do dziennika biblioteki
+    :param action:
+    :return:
+    """
+
+
+def validate_isbn(param_name: str = "isbn"):
+    def deco(fn):
+
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            if param_name in kwargs:
+                raw = str(kwargs[param_name])
+                clean = raw.replace("-", "").strip()
+                if len(clean) != 10 or not clean.isdigit():
+                    raise ValueError("ISBN musi miec dokłądnie 10 cyfr")
+                kwargs[param_name] = clean
+                return fn(*args, **kwargs)
+
+            if len(args) < 2:
+                raise ValueError("Brak argumentu iSBN.")
+
+            raw = str(args[1])
+            clean = raw.replace("-", "").strip()
+            if len(clean) != 10 or not clean.isdigit():
+                raise ValueError("ISBN musi miec dokłądnie 10 cyfr")
+            new_args = list(args)
+            new_args[1] = clean
+            return fn(*tuple(new_args), **kwargs)
+
+        return wrapper
+
+    return deco
+
 
 class Book:
     def __init__(self, title, author, isbn):
@@ -40,6 +79,7 @@ class Library:
                 return book
         raise Exception("Ksiązka nie znaszej biblioteki")
 
+    @validate_isbn("isbn")
     def fun_wypozycz_ksiazke(self, isbn):
         for book in self.dostepne_ksiazki:
             if book.isbn == isbn:
