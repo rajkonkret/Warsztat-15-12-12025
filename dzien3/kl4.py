fca9c89d2481c341f6ce8001d681cefbc66ced54
Radek <rajkonkret660@gmail.com>
2025-12-17 11:58:43 +0100

    [3 17] update kl4

--- dzien3/kl4.py
+++ dzien3/kl4.py
@@ -14,3 +14,22 @@ d1 = DefaultDict()
 print(type(d1))  # <class '__main__.DefaultDict'>
 print(d1)  # {}
 print(d1['name'])  # default
+
+
+# słownikw którym, gdy nie ma klucza, tworzy taki klucz z wartością domyślną np.: 0
+class AutoKeyDict(dict):
+    def __missing__(self, key):
+        self[key] = 0
+        return key
+
+
+a1 = AutoKeyDict()
+print(a1)  # {}
+print(a1['name'])  # name
+print(a1)  # {'name': 0}
+
+a2 = AutoKeyDict()
+a2['age'] = 34
+print(a2)  # {'age': 34}
+print(a2['name'])
+print(a2)  # {'age': 34, 'name': 0}
