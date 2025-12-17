1b5a278d4f6ffbab3e3dc054d8d40dacb07166e9
Radek <rajkonkret660@gmail.com>
2025-12-17 12:43:58 +0100

    [3 22] update kl7

---
+++ dzien3/kl7.py
@@ -0,0 +1,12 @@
+# stworzenie ksiązki telefonicznej z wykorzystaniem petli while
+# dodaj kontakt, usun kontakt, wyszukaj kontakt, wyswietl kontakt - klas
+
+# -----
+# Stworzyc system zarządzania biblioteką
+# Book
+# 3 dodanie ksiązek, wypozyczenie, zwrot ksiazki
+# lista dostepnych
+# lista wypożyczonych
+# dodac kalsę Library i ew. usera
+# title, author, isbn
+# obsłużyć błędy
