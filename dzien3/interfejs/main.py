102b46924d2828bc40a76efced5533a7cc0bd437
Radek <rajkonkret660@gmail.com>
2025-12-17 15:52:44 +0100

    [3 39] update interface_zad1.py

--- dzien3/interfejs/main.py
+++ dzien3/interfejs/main.py
@@ -10,3 +10,4 @@ print(f"Spalanie [l/100km]: {p1.spalanie(odl, jedn):.2f}")
 print(f"Koszt przejazdu na odcinku {odl} km wynosi: {p1.koszty_przejazdu(odl, jedn, cj):2f} pln")
 # Spalanie [l/100km]: 8.69
 # Koszt przejazdu na odcinku 587 km wynosi: 335.070000 pln
+# https://naukapythona.com.pl/
