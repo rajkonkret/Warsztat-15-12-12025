a503a840390280d2b3ed3689e32b3f6c12203f10
Radek <rajkonkret660@gmail.com>
2025-12-17 14:52:39 +0100

    [3 29] update abstrakcja

--- dzien3/abstrakcja/main.py
+++ dzien3/abstrakcja/main.py
@@ -1,5 +1,6 @@
 from prototyp import Prototyp
 from xyz import XYZ
+from regular import Regular
 
 # nie mozna stworzyc obiektu klasy abstrakcyjnej
 # TypeError: Can't instantiate abstract class Prototyp without an implementation
@@ -12,3 +13,21 @@ print(f"Policz: {xyz1.policz()}")
 # xyz 001xyz 001xyz 001
 # Policz: 5
 xyz1.msg()  # Metoda nieabstrakcyjna klasy abstrakcyjnej
+
+rg = Regular(3, 6)
+print(rg.info("Tekst"))
+print(f"Policz: {rg.policz()}")
+rg.msg()
+# Wiadomość: Tekst
+# Policz: 2.0
+# Metoda nieabstrakcyjna klasy abstrakcyjnej
+
+lista = [rg, xyz1]  # obiekty różnych klas
+# polimorfizm
+# obiekty róznych klas możemy potraktować jak
+# obiekty jednej wspólnej klasy
+# zapewnia nam to klasa abstrakcyjna
+for i in lista:
+    print(i.policz())
+# 2.0
+# 5
