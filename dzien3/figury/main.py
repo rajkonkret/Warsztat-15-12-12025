ab5b8899464f6c38c34f76d87a9db326ffa684da
Radek <rajkonkret660@gmail.com>
2025-12-17 15:34:52 +0100

    [3 33] update abstrakcja

--- dzien3/figury/main.py
+++ dzien3/figury/main.py
@@ -1,6 +1,22 @@
 # prostokąt, trapez, koło, kwadrat, trójkąt
-
+from prostokat import Prostokat
 from kwadrat import Kwadrat
 
 kw = Kwadrat(5)
 print(f"Pole figury {kw.__class__.__name__}: {kw.policz_pole()} ")
+
+print(50 * "-")
+pr1 = Prostokat(5, 10)
+print(f"Pole figury {pr1.__class__.__name__}: {pr1.policz_pole()} ")
+print(type(pr1))
+# --------------------------------------------------
+# Pole figury Prostokat: 50
+# <class 'prostokat.Prostokat'>
+
+print(50 * "-")
+pr2 = Prostokat(5, 5)
+print(f"Pole figury {pr2.__class__.__name__}: {pr2.policz_pole()} ")
+print(type(pr2))
+# --------------------------------------------------
+# Pole figury Kwadrat: 25
+# <class 'dzien3.figury.kwadrat.Kwadrat'>
