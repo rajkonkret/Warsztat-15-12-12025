97ab50ad908586e30b18609b0ec08515649956c6
Radek <rajkonkret660@gmail.com>
2025-12-18 11:40:05 +0100

    [4 19] update baza1.py

---
+++ dzien4/baza/baza2.py
@@ -0,0 +1,20 @@
+# context manager
+# ułatwia i pozwala na bezpieczniejszą praće z plikami, baza danych itd
+# with
+import sqlite3
+
+# __enter__, __exit__
+class SqliteCM:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
+
+    # wykona się zawsze
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.conn:
+            self.conn.commit()
+            self.conn.close()
