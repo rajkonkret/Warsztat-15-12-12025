9f18fd77546439886aba71629049465714169e42
Radek <rajkonkret660@gmail.com>
2025-12-18 12:41:39 +0100

    [4 26] update kl_operatory.py.py

--- dzien4/kl_operatory.py
+++ dzien4/kl_operatory.py
@@ -36,3 +36,27 @@ num5 = Number2(10)
 print(num5 == num4)
 
 print(num5 > num3)  # True
+
+print(5 + 5)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        if isinstance(other, Point):
+            return Point(self.x + other.x, self.y + other.y)
+        return NotImplemented
+
+    def __repr__(self):
+        return f"Point({self.x}, {self.y})"
+
+
+point1 = Point(1, 4)
+point2 = Point(7, 8)
+
+# print(point1 + point2) # TypeError: unsupported operand type(s) for +: 'Point' and 'Point'
+wynik = point2 + point1
+print("Wynik:", wynik)  # Wynik: Point(8, 12)
