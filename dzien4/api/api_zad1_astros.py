1d9d8b8aeff74f9128ea4835d5a4a972790b1b40
Radek <rajkonkret660@gmail.com>
2025-12-18 14:23:41 +0100

    [4 32] update api_zad_astros

--- dzien4/api/api_zad1_astros.py
+++ dzien4/api/api_zad1_astros.py
@@ -3,7 +3,10 @@
 # wykorzystujący standardowe żądania HTTP (GET, POST, PUT, DELETE)
 # do wymiany danych (najczęściej w formatach JSON/XML) między aplikacjami (klient-serwer),
 # umożliwiający prostą, skalowalną i zunifikowaną komunikację między systemami, działając jak "kelner" dostarczający dane.
+from typing import List
+
 import requests
+from pydantic import BaseModel
 
 url = "http://api.open-notify.org/astros.json"
 
@@ -16,10 +19,10 @@ print(response)  # <Response [200]>
 # https://pl.wikipedia.org/wiki/Kod_odpowiedzi_HTTP
 
 print(response.text)
-print(type(response.text)) # str
+print(type(response.text))  # str
 
 response_data = response.json()
-print(type(response_data)) # <class 'dict'>
+print(type(response_data))  # <class 'dict'>
 
 for i in response_data:
     print(i)
@@ -28,3 +31,28 @@ for i in response_data:
 # message
 
 print(response_data['people'])
+
+
+class Astronaut(BaseModel):
+    craft: str
+    name: str
+
+
+class AstrosData(BaseModel):
+    message: str
+    number: int
+    # number: str # pydantic_core._pydantic_core.ValidationError: 1 validation error for AstrosDat
+    # people: list
+    people: List[Astronaut]
+
+# zamiana na obiekty
+data = AstrosData(**response.json())
+print(data)
+print(type(data.people[0]))  # <class 'dict'>
+# po dodaniu List -> <class '__main__.Astronaut'>
+
+for astronaut in data.people:
+    print(f"{astronaut.name}, {astronaut.craft}")
+    # Oleg Kononenko, ISS
+    # Nikolai Chub, ISS
+    # Tracy Caldwell Dyson, ISS
