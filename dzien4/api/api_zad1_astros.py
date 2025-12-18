19379fb46323a01086b565541193c038108d97f8
Radek <rajkonkret660@gmail.com>
2025-12-18 14:09:55 +0100

    [4 30] update api_zad_astros

---
+++ dzien4/api/api_zad1_astros.py
@@ -0,0 +1,16 @@
+# REST API (Representational State Transfer Application Programming Interface
+# ) to styl architektoniczny dla budowania usług sieciowych,
+# wykorzystujący standardowe żądania HTTP (GET, POST, PUT, DELETE)
+# do wymiany danych (najczęściej w formatach JSON/XML) między aplikacjami (klient-serwer),
+# umożliwiający prostą, skalowalną i zunifikowaną komunikację między systemami, działając jak "kelner" dostarczający dane.
+import requests
+
+url = "http://api.open-notify.org/astros.json"
+
+response = requests.get(url)
+print(response)  # <Response [200]>
+# 200 ok
+# 3xx - warnngi przekierowania
+# 4xx - bład po stronie klienta, 404 Page Not Found, 400 Bad Request
+# 5xx - błedy po stronie serwera
+# https://pl.wikipedia.org/wiki/Kod_odpowiedzi_HTTP
