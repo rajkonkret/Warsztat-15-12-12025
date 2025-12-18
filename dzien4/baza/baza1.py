9e9f7dc80cddf37de9a527ccd485143c639fcbbf
Radek <rajkonkret660@gmail.com>
2025-12-18 11:35:55 +0100

    [4 18] update baza1.py

---
+++ dzien4/baza/baza1.py
@@ -0,0 +1,18 @@
+# baza danych - silnik bazy danych
+# sql
+# mysql, postgres, mssql, oracle
+
+import sqlite3
+
+try:
+    sql_connection = sqlite3.connect('sqlite_db.db')
+    cursor = sql_connection.cursor()
+    print("Baza została podłączona")
+except sqlite3.Error as e:
+    print("Bład podłaczennia bazy danych")
+finally:
+    if sql_connection:
+        sql_connection.close()
+# Baza została podłączona
+#
+# Process finished with exit code 0
