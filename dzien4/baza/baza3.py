466aa3cfefaf8a8266f1c46354b43a4cdd425765
Radek <rajkonkret660@gmail.com>
2025-12-18 12:11:18 +0100

    [4 23] update baza3.py

--- dzien4/baza/baza3.py
+++ dzien4/baza/baza3.py
@@ -6,7 +6,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 
-engine = create_engine('sqlite:///moja_baza.db')
+engine = create_engine('sqlite:///moja_baza.db', echo=True)
 Base = declarative_base()
 
 
@@ -21,3 +21,27 @@ class Person(Base):
 
 
 Base.metadata.create_all(engine)
+# 2025-12-18 12:05:44,864 INFO sqlalchemy.engine.Engine BEGIN (implicit)
+# 2025-12-18 12:05:44,864 INFO sqlalchemy.engine.Engine PRAGMA main.table_info("person")
+# 2025-12-18 12:05:44,864 INFO sqlalchemy.engine.Engine [raw sql] ()
+# 2025-12-18 12:05:44,865 INFO sqlalchemy.engine.Engine COMMIT
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+person = Person(name="Radek", age="23")
+session.add(person)
+session.commit()
+# INSERT INTO person (name, age) VALUES (?, ?)
+
+persons = session.query(Person).all()
+print(persons)
+# SELECT person.id AS person_id, person.name AS person_name, person.age AS person_age
+# FROM person
+
+for p in persons:
+    print(p.name)
+# [Radek, Radek, Radek]
+# Radek
+# Radek
+# Radek
